c98c95fd5a0b274d3caa8dc82693710fcb0d5857
Ndeye Fatou Diop <ndeye-fatou.diop@polytechnique.edu>
2017-05-09 17:07:17 +0200

    Add delete message

--- src/components/account/DeleteAccount.js
+++ src/components/account/DeleteAccount.js
@@ -2,11 +2,16 @@
  * Created by diop on 09/05/2017.
  */
 import React from 'react';
-import { Button, Form, Divider, Input, Select, Segment } from 'semantic-ui-react'
+import { Button, Segment, Header } from 'semantic-ui-react'
 
 const DeleteAccount = props => (
-    <Segment>
-        Suppression de votre compte HAIRDV
+    <Segment color="red">
+        <Header as='h3'>Suppression de votre compte HAIRDV</Header>
+        <p>
+            Si vous ne souhaitez plus utiliser HairDV, vous avez la possibilité de supprimer votre compte.<br/><br/>
+            Cette action irréversible aura pour effet d'effacer vos données personnelles et de connexion de la base de données d'HairDV.
+        </p>
+        <Button color='red'>Supprimer mon compte</Button>
     </Segment>
 );
 
